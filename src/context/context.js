ef0d5af1d20204321cf5707ee9fac565099a8142
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2023-01-24 21:08:57 +0000

    info component added

--- src/context/context.js
+++ src/context/context.js
@@ -9,7 +9,13 @@ const rootUrl = 'https://api.github.com';
 const GithubContext = React.createContext();
 
 const GithubProvider = ({children}) => {
-  return <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  const [ githubUser, setGithubUser ] = useState(mockUser);
+  const [ repos, setRepos ] = useState(mockRepos);
+  const [ followers, setFollowers ] = useState(mockFollowers);
+
+  //console.log(githubUser);
+
+  return <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
 };
 
 export { GithubProvider, GithubContext };
